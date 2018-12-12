527ed3a4b42096744a2522c8a6b4762a173a77f4
hassan-maavan <hassan@maavan.com>
2018-12-13 00:40:55 +0500

    Contacts Module

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -12,4 +12,10 @@ class ContactsController extends Controller
     public function create(){
         return view('contacts.create');
     }
+    public function save_contact(Request $request){
+        $validatedData = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+        ]);
+    }
 }
