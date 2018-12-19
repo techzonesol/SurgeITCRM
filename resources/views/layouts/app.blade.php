84aac58ed11df93f8c6a95f92e2547b670580dd1
hassan-maavan <hassan@maavan.com>
2018-12-19 23:48:49 +0500

    Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=0"/>
-  <link href="css/jquery-ui.1.11.2.min.css" rel="stylesheet" />
+  <link href="{{asset('css/jquery-ui.1.11.2.min.css')}}" rel="stylesheet" />
   <script>
     window.onload = function () {
 
@@ -156,26 +156,6 @@
 <section class="gnrl-blk gnrl-mrgn-pdng card-playing">
     @yield('content')
 </section><!-- card playing end -->
-
-
-
-
-
-
-
-
-<footer>
-  <div class="copy-content">
-    <div class="container">
-      <a href="#" target="_blank">
-        <p>
-          &copy; Copyright 2018. All Rights Reserved.
-
-        </p>
-      </a>
-    </div>
-  </div>
-</footer>
 
 
 
