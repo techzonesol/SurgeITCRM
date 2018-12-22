70cbfc61d12a6d7b1baf54b9a785effb186a1802
hassan-maavan <hassan@maavan.com>
2018-12-23 00:05:53 +0500

    Register Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -80,7 +80,7 @@ class RegisterController extends Controller
             'user_username' => $data['user_username'],
             'user_email' => $data['user_email'],
             'password' => Hash::make($data['user_password']),
-            'user_dob' => $data['user_dob'],
+            'user_dob' => date('Y-m-d',strtotime($data['user_dob'])),
             'user_cell_no' => $data['user_cell_no'],
             'user_home_phone' => $data['user_home_phone'],
             'user_city' => $data['user_city'],
