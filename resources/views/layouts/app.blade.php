3482d499aca105f4b8d1f319ddc5f2853d8f10f1
hassan-maavan <hassan@maavan.com>
2019-01-02 21:08:41 +0500

    Add no-index and no-follow meta titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>@yield('meta_title') | {{env('PROJECT_NAME')}}</title>
+	<meta name="robots" content="noindex, nofollow">
 
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
